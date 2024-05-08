73a6ef94515f07bafecc840b73251559a54babb5
sai <girgaonkar.chaitanya@gmail.com>
2024-05-08 20:20:57 +0530

    filed folder added

--- backend_webWizards/src/middlewares/multer.middleware.js
+++ backend_webWizards/src/middlewares/multer.middleware.js
@@ -3,8 +3,8 @@ import multer from "multer";
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         // Log destination directory
-        console.log("Destination directory:", "./public/temp");
-        cb(null, "./src/files");
+        // console.log("Destination directory:", "./files");
+        cb(null, "./files");
     },
     filename: function (req, file, cb) {
         // Log original filename
